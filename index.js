be77bbdd440946997a19399bea8b14d928e3aa02
Sumaiya Urmi <akthersumaiya689@gmail.com>
2024-08-18 13:59:31 +0600

    pagination functionality created

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: [
-      "http://localhost:5173",
-      "https://qucikcart-cc6c1.web.app",
-      "https://qucikcart-cc6c1.firebaseapp.com"
-  
-    ],
-    credentials: true,
-    optionSuccessStatus: 200,
-  };
-  // middleware
-  app.use(cors(corsOptions));
-  app.use(express.json());
-
+  origin: [
+    "http://localhost:5173",
+    "https://qucikcart-cc6c1.web.app",
+    "https://qucikcart-cc6c1.firebaseapp.com",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+// middleware
+app.use(cors(corsOptions));
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aea2zks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -28,7 +26,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -38,16 +36,32 @@ async function run() {
 
     const productCollection = client.db("quickCart").collection("products");
 
-  // products apis
-  app.get("/products", async (req, res) => {
-    const result = await productCollection.find().toArray();
-    res.send(result);
-  });
+    // products apis
+    //   get all products
+    app.get("/products", async (req, res) => {
+      const size = parseInt(req.query.size);
+      const page = parseInt(req.query.page) - 1;
+      console.log(page, size);
 
+      const result = await productCollection
+        .find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
+
+    //   get all products count
+    app.get("/products-count", async (req, res) => {
+      const count = await productCollection.countDocuments();
+      res.send({ count });
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -55,11 +69,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send("QuickCart server is running");
-  });
-  app.listen(port, () => {
-    console.log(`QuickCart running on port ${port}`);
-  });
-  
+  res.send("QuickCart server is running");
+});
+app.listen(port, () => {
+  console.log(`QuickCart running on port ${port}`);
+});
